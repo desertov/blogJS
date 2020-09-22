e0b21e81f928aff4a321507b3d6e8e966646c7ed
Victor Deserto <victor_deserto@hotmail.com>
2020-09-21 22:39:14 -0400

    Commitando com package-lock.json

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json())
 
 app.listen(3000 || process.env.PORT, () =>{
     console.log('Estou ouvindo')
