3437006c510a6b927ac79dd9c178d596b487a9cc
Victor Deserto <victor_deserto@hotmail.com>
2020-09-21 22:28:05 -0400

    Primeiro commit app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-a
+//Configurações gerais
+
+import express from 'express'
+import handlebars from 'express-handlebars'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.listen(3000 || process.env.PORT, () =>{
+    console.log('Estou ouvindo')
+})
