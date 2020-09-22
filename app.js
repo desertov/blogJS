96be372dca90da1cd7706ef884dc4fceb59563a5
Victor Deserto <victor_deserto@hotmail.com>
2020-09-21 23:53:12 -0400

    Adicionando routes

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ import bodyParser from 'body-parser'
 
 const app = express()
 
-// app.use(bodyParser.urlencoded({extended: true}))
-// app.use(bodyParser.json())
+//Configurando o BodyParser
+ app.use(bodyParser.urlencoded({extended: true}))
+ app.use(bodyParser.json())
+
+//Configurando o HandleBars
 
 app.listen(3000 || process.env.PORT, () =>{
     console.log('Estou ouvindo')
+})
+
+app.get('/', (req, res) =>{
+    res.send('Página Inicial')
+})
+
+app.get('/documentacao', (req, res) =>{
+    res.send('Documentação')
 })
